71c9ea59287e5ca42a7054a33360bb1b49d8e8b7
djiang2 <djiang2>
2018-09-27 10:58:28 -0400

    Created .h file

---
+++ mystr.h
@@ -0,0 +1,11 @@
+
+int strlen(char *)];
+
+char * strcpy( char *dest, char *source )  char * strncpy( char *dest, char *source, int n);
+
+int strcmp( char *s1, char *s2 );
+
+
+char * strchr( char *s, char c );
+
+char * strstr( char *s1, char * s2 ) ;
